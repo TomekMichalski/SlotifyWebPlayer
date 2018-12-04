0bf66994383e3cccdc97d42c4fbb9331b8d01ee8
TomekMichalski <tmm.michalski@gmail.com>
2018-12-04 16:58:08 +0100

    Web Music Player base

---
+++ includes/classes/Constants.php
@@ -0,0 +1,16 @@
+<?php
+    class Constants {
+        public static $passwordsDoNotMatch = "Your passwords don't match.";
+        public static $passwordNotAlphanumeric = "Your password can only contain alphanumeric symbols.";
+        public static $passwordCharacters = "Your password must be between 5 and 30 characters long.";
+        public static $emailInvalid = "Email is invalid.";
+        public static $emailsDoNotMatch = "Your emails don't match.";
+        public static $emailTaken = "Your email is already used.";
+        public static $lastNameCharacters = "Your last name must be between 2 and 25 characters long.";
+        public static $firstNameCharacters = "Your first name must be between 2 and 25 characters long.";
+        public static $usernameCharacters = "Your username must be between 5 and 25 characters long.";
+        public static $usernameTaken = "This username is already taken.";
+
+        public static $loginFailed = "Your username or password was incorrect";
+    }
+?>
